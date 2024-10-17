6ca721a218a8823b6ddefc63fd96f81e52a8bb7b
iamkodenaime <creativedannyk@gmail.com>
2024-10-17 06:43:39 +0100

    main hero page design

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
+import heroimage from '../Assets/heroimage.jpg'
 
 import '../Pages/Home/home.css'
 
 const Hero = () => {
   return (
-    <section className="container hero-container">
-        hero
-    </section>
+    <header className="hero-wrapper">
+        <div className="container hero-container">
+            <div className="hero-left">
+                <h4>We Aim to Meet your Furniture Needs</h4>
+                <h1>Get in touch with us today</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis dolore placeat, fuga obcaecati quae odit!
+                </p>
+                <Link to="/gallery" className='btn' >Explore</Link>
+            </div>
+            <div className="hero-right">
+                <div className="hero-circle"></div>
+                <img src={heroimage} alt="" />
+            </div>
+        </div>
+    </header>
   )
 }
 
