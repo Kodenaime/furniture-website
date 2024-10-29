962ae8f68b9c79103e0e5bb88ee466a6f5b41605
iamkodenaime <creativedannyk@gmail.com>
2024-10-29 13:24:31 +0100

    finished with the gallery page

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+
+import Header from '../../Components/Header'
+import headerImage from '../../Assets/header_bg_3.jpg'
+
 import './gallery.css'
 
 const Gallery = () => {
+
+  const galleryLenght = 15;
+  const images = []
+
+  for(let i = 1; i <= galleryLenght; i++) {
+    images.push(require(`../../Assets/gallery${i}.jpg`))
+  }  console.log(images);
+
   return (
-    <div>Gallery</div>
+    <>
+      <Header title="Our gallery" image={ headerImage }>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Laudantium, unde?
+      </Header>
+      <section className="gallery">
+        <div className="container gallery-container">
+          {
+            images.map((image, index) => {
+              return <article key={index}>
+                  <img src={image} alt={`Gallery Images ${index + 1}`} />
+              </article>
+            })
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
